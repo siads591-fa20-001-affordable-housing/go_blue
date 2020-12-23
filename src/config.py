983085ccb87bd0ebf82e7ee290823a4838a61793
hokoonleong <75458167+hokoonleong@users.noreply.github.com>
2020-12-23 22:37:48 +0800

    Delete config.py

--- src/config.py
+++
@@ -1,9 +0,0 @@
-def dbconfig():
-    return {"host": "affordablehousing.a2hosted.com",
-            "port": 5432,
-            "database": "afford31_housing",
-            "user": "afford31_siads",
-            "pass": "NpSs7M!vAHhQrD1Z8e@!N7*kQZZ4"}
-
-
-
