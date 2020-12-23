fb5d303de31f99c55747fa4b90d4dc772b01a25e
hokoonleong <75458167+hokoonleong@users.noreply.github.com>
2020-12-23 17:24:48 +0800

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,9 @@
+def dbconfig():
+    return {"host": "affordablehousing.a2hosted.com",
+            "port": 5432,
+            "database": "afford31_housing",
+            "user": "afford31_siads",
+            "pass": "NpSs7M!vAHhQrD1Z8e@!N7*kQZZ4"}
+
+
+
