2ed60183f986796fb7a8a971ac2e52480d6ca9ef
Ho Koon Leong <koonleong@outlook.com>
2021-01-31 01:14:42 +0800

    Preparing FINAL files

---
+++ src/final/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,21 @@
+# +
+from sqlalchemy import create_engine
+import psycopg2
+
+def dbconfig():
+    
+    db = {"host": "affordablehousing.a2hosted.com",
+            "port": 5432,
+            "database": "afford31_housing",
+            "user": "afford31_siads",
+            "pass": "NpSs7M!vAHhQrD1Z8e@!N7*kQZZ4"}
+    pg_string = f"postgresql://{db['user']}:{db['pass']}@{db['host']}:{db['port']}/{db['database']}"
+    database_engine = create_engine(pg_string, echo = False)
+    return database_engine
+
+
+# -
+def basedir():
+    import os
+    return os.getcwd()
+
